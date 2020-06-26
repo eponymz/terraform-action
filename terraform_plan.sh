56de4634721e0641a0c6d965d9f10503f3fa70c7
Ian Sabey <ian@cogni.is>
2020-06-26 13:08:32 -0600

    release version v1.0.9

--- terraform_plan.sh
+++ terraform_plan.sh
@@ -3,6 +3,17 @@
 TFPATH=$1
 REGION=$2
 ACTION=$3
+PR_NUMBER=$("$GITHUB_EVENT_PATH" | jq -r ".pull_request.number")
+REPO_OWNER="cogni-dev"
+REPO_NAME="infra"
+PR_URL="https://api.github.com/repos/${REPO_OWNER}/${REPO_NAME}/issues/${PR_NUMBER}/comments"
+
+destructive_plan () {
+  COMMENT_BODY='{"body": "Destroy actions present in '$1'. Please review the related workflow execution to ensure this is intended!"}'
+  echo " Commenting on PR."
+  curl -s -H "Authorization: token ${GITHUB_API_TOKEN}" -X POST -d $COMMENT_BODY $PR_URL
+  EXITCODE=0
+}
 
 if [ -z "$REGION" ] || [ -z "$TFPATH" ] || [ -z "$ACTION" ] ; then
   echo "Please set all variables"
@@ -28,14 +39,14 @@ fi
 
 case $ACTION in
   plan)
-    ACTION="plan -lock=false"
+    ACTION="plan -lock=false -detailed-exitcode -out=plan.tmp"
   ;;
   apply)
     ACTION="apply -auto-approve"
   ;;
   *)
     echo "action not specificed, defaulting to plan"
-    ACTION="plan -lock=false"
+    ACTION="plan -lock=false -detailed-exitcode -out=plan.tmp"
   ;;
 esac
 
@@ -44,11 +55,30 @@ echo "ACTION is '$ACTION'"
 cd $TFPATH
 
 terraform init
-[ $? -ne 0 ] && echo "Terraform initialization failed. Exiting.." && exit 1
+
 terraform fmt -check
 [ $? -ne 0 ] && echo "Unformatted Terraform files found. Please run 'terraform fmt' from within '$TFPATH' and push the changes. Exiting.." && exit 1
+
 terraform validate
 [ $? -ne 0 ] && echo "Terraform validation failed. Please run 'terraform validate' locally and resolve the issues mentioned. Exiting.." && exit 1
+
 terraform $ACTION
-[ $? -ne 0 ] && echo "Terraform '$ACTION' failed. Address issues in error output. Exiting.." && exit 1
-exit 0
+
+ACTION_EXIT_CODE=$?
+
+if [ $ACTION_EXIT_CODE -eq 1 ]; then
+  EXITCODE=1
+elif [ $ACTION_EXIT_CODE -eq 2 ] && [[ $ACTION =~ plan ]]; then
+  for i in $(tf show -json plan.tmp | jq -r ".resource_changes[].change.actions[]"); do
+    if [ $i = "delete" ]; then
+      DESTRUCTIVE_PLAN=true
+      break
+    fi
+  done
+
+  [ $DESTRUCTIVE_PLAN = "true" ] && destructive_plan $TFPATH
+else
+  EXITCODE=0
+fi
+
+exit $EXITCODE
