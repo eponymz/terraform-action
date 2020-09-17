156d7e2dc2347fa8ee2b31d01821e6a739e59557
Ian Sabey <ian@cogni.is>
2020-09-17 10:32:06 -0600

    release version v1.3.1

--- terraform_plan.sh
+++ terraform_plan.sh
@@ -25,7 +25,7 @@ destructive_plan () {
     CURL_COMMAND=$(curl -sSw "%{response_code}" -H "Authorization: token ${ACCESS_TOKEN}" -X POST -d "$COMMENT_BODY" $PR_URL)
   fi
   echo "$CURL_COMMAND"
-  if [ "$response_code" = "200" ] || [ "$response_code" = "201" ]; then # Slack sends 200 on successful call. GitHub sends 201 on successful call.
+  if [ $response_code = "200" ] || [ $response_code = "201" ]; then # Slack sends 200 on successful call. GitHub sends 201 on successful call.
     EXITCODE=0
   else
     EXITCODE=1
