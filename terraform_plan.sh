cffb188c0f003674f06776cf6693d8bbaf20c999
Ian Sabey <ian@cogni.is>
2020-09-17 12:59:27 -0600

    release version v1.4.0

--- terraform_plan.sh
+++ terraform_plan.sh
@@ -20,10 +20,10 @@ destructive_plan () {
   local CURL_COMMAND
   if [[ $IS_MANUAL = true ]]; then
     echo "Sending Slack Message."
-    CURL_COMMAND=$(curl -o /dev/null -w "%{response_code}" -sS -H "Content-type: application/json" -X POST -d "$SLACK_MESSAGE_BODY" $SLACK_WEBHOOK_URL)
+    CURL_COMMAND=$(curl -s -o /dev/null -w "%{response_code}" -H "Content-type: application/json" -X POST -d "$SLACK_MESSAGE_BODY" $SLACK_WEBHOOK_URL)
   else
     echo "Commenting on PR at '$PR_URL'."
-    CURL_COMMAND=$(curl -o /dev/null -w "%{response_code}" -sS -H "Authorization: token ${ACCESS_TOKEN}" -X POST -d "$COMMENT_BODY" $PR_URL)
+    CURL_COMMAND=$(curl -s -o /dev/null -w "%{response_code}" -H "Authorization: token ${ACCESS_TOKEN}" -X POST -d "$COMMENT_BODY" $PR_URL)
   fi
   if [ $CURL_COMMAND != "200" -o $CURL_COMMAND != "201" ]; then # Slack sends 200 on successful call. GitHub sends 201 on successful call.
     EXITCODE=1
